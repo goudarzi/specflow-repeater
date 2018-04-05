670c6bcb783712209939a4171b9593a3cb1d6135
Rikki Goudarzi <10061151+goudarzi@users.noreply.github.com>
2018-04-06 00:23:33 +1200

    Added Plugin class.

---
+++ Repeater.SpecflowPlugin/SpecflowRepeaterPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow.Generator.Plugins;
+
+namespace Repeater.SpecflowPlugin
+{
+	public class SpecflowRepeaterPlugin
+		: IGeneratorPlugin
+	{
+		void IGeneratorPlugin.Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters)
+		{
+			generatorPluginEvents.CustomizeDependencies += GeneratorPluginEvents_CustomizeDependencies;
+		}
+
+		private void GeneratorPluginEvents_CustomizeDependencies(object sender, CustomizeDependenciesEventArgs e)
+		{
+		}
+	}
+}
